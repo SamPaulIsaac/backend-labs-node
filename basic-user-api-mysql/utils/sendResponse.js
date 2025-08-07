522241ddb39c90bab472851b18c4656b7ac0c961
SamPaul Isaac <sampaulisaac18@outlook.com>
2025-08-07 18:02:20 +0530

    @SamPaulIsaac: Renamed directories.

--- basic-user-api-mysql/utils/sendResponse.js
+++
@@ -1,6 +0,0 @@
-function sendResponse(res, statusCode, data) {
-  res.writeHead(statusCode, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(data));
-}
-
-module.exports = { sendResponse };
